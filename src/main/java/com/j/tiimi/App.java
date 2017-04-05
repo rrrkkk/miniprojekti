e8eef800193c0cd7ed33993a918a1ca29614528b
JoonasSa <joonas.sarapalo@gmail.com>
2017-04-05 18:19:37 +0300

    Bibtex: /reference, Latex seed: /test

--- src/main/java/com/j/tiimi/App.java
+++ src/main/java/com/j/tiimi/App.java
@@ -1,36 +1,13 @@
 package com.j.tiimi;
 
-import com.j.tiimi.entity.Attribute;
-import com.j.tiimi.entity.Reference;
-import com.j.tiimi.latex.LatexGenerator;
-import com.j.tiimi.service.ReferenceService;
-import java.util.ArrayList;
-import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        //SpringApplication.run(App.class, args);
-
-        makeLatex();
+        SpringApplication.run(App.class, args);
     }
     
-    public static void makeLatex() {
-        ReferenceService service = new ReferenceService();
-        Reference reference = new Reference();
-        reference.setIdentifier("asd");
-        reference.setType("Book");
-        ArrayList<Attribute> attributes = new ArrayList();
-        attributes.add(new Attribute().setAttributes("Author", "Matti Lokki"));
-        attributes.add(new Attribute().setAttributes("Year", "1998"));
-        attributes.add(new Attribute().setAttributes("MAmbo", "Jambo"));
-        attributes.add(new Attribute().setAttributes("Kirja", "on se"));
-        reference.setAttributes(attributes);
-        List<Reference> list = new ArrayList<Reference>();
-        list.add(reference);
-        System.out.println(new LatexGenerator().getString(list));
-    }
 }
--- src/main/java/com/j/tiimi/controller/ReferenceController.java
+++ src/main/java/com/j/tiimi/controller/ReferenceController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping("reference")
@@ -15,12 +18,20 @@ public class ReferenceController {
     @Autowired
     ReferenceService referenceService;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
-    public List<Reference> list() {
-        return referenceService.listReferences();
-        //return referenceService.getBibtexString(); TÄÄ!
+    public String bibtex() {
+        return referenceService.getBibtexString();
     }
+
+    /* Maybe better way to do it, dunno...
+    @RequestMapping(method = RequestMethod.GET)
+    public HttpEntity<String> bibtex() {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("MyResponseHeader", "MyValue");
+        return new HttpEntity<>(referenceService.getBibtexString(), responseHeaders);
+    }
+    */
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
